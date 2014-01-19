3d99ca97d4065cc36b23062ba236b33d743053e4
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-01-19 13:53:49 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nextup::Application.config.secret_key_base = '39481b99fcb5e2390ff6e11eb57f9320d1719b0665956a6c7e6919f7872220c909be53a4ba56c73b4c44b5628e77d53c66f684be9a43f3411a381585c40aed64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nextup::Application.config.session_store :cookie_store, key: '_nextup_session'
